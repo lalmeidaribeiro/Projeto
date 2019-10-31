280775b342239d0db336dceac0ad713829f17228
lalmeidaribeiro <rlarissa191@gmail.com>
2019-10-31 14:14:59 -0200

    Criação do Index e do CSS-Padrão.

--- aplicacao.py
+++ aplicacao.py
@@ -6,24 +6,4 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/contato')
-def contato():
-    return render_template("contato.html")
-
-@app.route('/detalhecurso')
-def detalhecurso():
-    return render_template("detalhecurso.html")
-
-@app.route('/disciplina')
-def disciplina():
-    return render_template("disciplina.html")
-
-@app.route('/listacursos')
-def listacursos():
-    return render_template("listacursos.html")
-
-@app.route('/noticias')
-def noticias():
-    return render_template("noticias.html")
-
 app.run()
